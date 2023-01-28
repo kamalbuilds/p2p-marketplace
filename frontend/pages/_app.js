d1415049b5f51f01765dcf8f8976ef5fab6b2d39
legendarykamal <geniusamansingh@gmail.com>
2023-01-28 11:05:18 +0530

    resolving the syntax error

---
+++ frontend/pages/_app.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import "../styles/globals.css";
+
+// import WalletConnectionProvider from "../components/WalletConnectionProvider";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <title>Cash App</title>
+      </Head>
+      <WalletConnectionProvider>
+        <Component {...pageProps} />
+      </WalletConnectionProvider>
+    </>
+  );
+}
+
+export default MyApp;
